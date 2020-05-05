05a6291325fa4887753d7622fe72731c6c2adca1
asdnf <33ppk@mail.ru>
2020-05-05 16:25:51 +0300

    1

--- src/main/java/org/ppk/accounts/accounts/AppConfig.java
+++ src/main/java/org/ppk/accounts/accounts/AppConfig.java
@@ -74,7 +74,7 @@ public class AppConfig {
     @Bean
     public Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.service:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
 //        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 //        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
@@ -87,7 +87,7 @@ public class AppConfig {
     @Bean
     public Map<String, Object> senderProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.service:9092");
 //        props.put(ProducerConfig.RETRIES_CONFIG, 0);
 //        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
 //        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
